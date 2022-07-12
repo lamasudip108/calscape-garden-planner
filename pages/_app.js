19e4d4085df21953627784c247b7e9d9c0b9fcc5
raisaken <saken.rai.34@gmail.com>
2022-07-12 20:29:09 +0545

    leaflet configured and map page added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import '../styles/globals.css'
-import '../styles/navigation.css'
+import "../styles/globals.css";
+import "../styles/navigation.css";
+import "../styles/map.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
